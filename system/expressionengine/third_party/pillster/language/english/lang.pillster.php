edf5ddf063b7ab4b2481d07ff22ec5a43889bf28
TJ Draper <tj@buzzingpixel.com>
2016-02-20 18:17:16 -0600

    Clear out all files to start build over

--- system/expressionengine/third_party/pillster/language/english/lang.pillster.php
+++
@@ -1,7 +0,0 @@
-<?php if (! defined('BASEPATH')) exit('No direct script access allowed');
-
-include(PATH_THIRD . 'pillster/config.php');
-
-$lang = array(
-	'status_color_description' => 'Use ExpressionEngine&rsquo;s status colors on the entry form',
-);
