9ce924c35128e673a26733f9c1ef360d83c16c18
TJ Draper <tj@buzzingpixel.com>
2016-03-11 08:24:45 -0600

    Fix an issue where Pillster won’t play nice with other extensions loaded before on the same hook

--- system/user/addons/pillster/language/english/lang.pillster.php
+++ system/user/addons/pillster/language/english/lang.pillster.php
@@ -1,5 +1,3 @@
 <?php if (! defined('BASEPATH')) exit('No direct script access allowed');
 
-$lang = array(
-	'status_color_description' => 'Use ExpressionEngine&rsquo;s status colors on the entry form',
-);
+$lang = array();
